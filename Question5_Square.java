b9abbf22796e8c3903f88d1544a7b6544b3396e5
ismail-sdet <71407703+ismail-sdet@users.noreply.github.com>
2020-09-22 23:21:37 +0200

    Add files via upload

---
+++ Question5_Square.java
@@ -0,0 +1,18 @@
+package assigments.day_2;
+
+public class Question5_Square {
+
+	public static void main(String[] args) {
+		
+		System.out.println("*******");
+		System.out.println("**   **");
+		System.out.println("* * * *");
+		System.out.println("*  *  *");
+		System.out.println("* * * *");
+		System.out.println("**   **");
+		System.out.println("*******");
+		
+
+	}
+
+}
---
+++ Question8_ID.java
@@ -0,0 +1,20 @@
+package assigments.day_2;
+
+public class Question8_ID {
+
+	public static void main(String[] args) {
+		System.out.println();
+		System.out.println("   **      ******");
+		System.out.println("   **      **    **");
+		System.out.println("   **      **     **");
+		System.out.println("   **      **      **");
+		System.out.println("   **      **      **");
+		System.out.println("   **      **      **");
+		System.out.println("   **      **     **");
+		System.out.println("   **      **    **");
+		System.out.println("   **      ******");
+		
+		
+	}
+
+}
