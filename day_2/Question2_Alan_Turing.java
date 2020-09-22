f665b044cc61ab294ea17d725586da3f49fea98c
ismail-sdet <71407703+ismail-sdet@users.noreply.github.com>
2020-09-22 23:25:45 +0200

    Add files via upload

---
+++ day_2/Question2_Alan_Turing.java
@@ -0,0 +1,10 @@
+package assigments.day_2;
+
+public class Question2_Alan_Turing {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Turing, Alan");
+	}
+
+}
---
+++ day_2/Question6_Empire_State.java
@@ -0,0 +1,22 @@
+package assigments.day_2;
+
+public class Question6_Empire_State {
+
+	public static void main(String[] args) {
+		
+		System.out.println("    ^");
+		System.out.println("  /   \\");
+		System.out.println(" /     \\");
+		System.out.println("/       \\");
+		System.out.println("|       |");
+		System.out.println("|       |");
+		System.out.println("|       |");
+		System.out.println("|       |");
+		System.out.println("|       |");
+		System.out.println("|       |");
+		System.out.println("|       |");
+		System.out.println("---------");
+
+	}
+
+}
